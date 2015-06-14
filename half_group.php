18106bd2bfd25efd709488fe76e0e80a94f9b4e8
aaron <aaron.ido@gmail.com>
2015-06-14 23:39:58 +0800

    stone package problem

---
+++ half_group.php
@@ -0,0 +1,88 @@
+<?php
+$a = [3,8,1,2,15,15,11,20,31];
+$max = 30;
+$result = match($a, $max);
+var_dump($result);
+/**
+ * 先对数组里的数分组
+ * 比max大的
+ * 比max/2大的一组
+ * 比max/2小的一组
+ * @return [type] [description]
+ */
+function half_group(array $a, $max){
+	$surpass = array();
+	$high = array();
+	$middle = array();
+	$low = array();
+	arsort($a, SORT_NUMERIC);
+	foreach ($a as $value) {
+		if ($value>=$max) {
+			$surpass[] = $value;
+		} elseif ($value*2>$max) {
+			$high[] = $value;
+		} elseif ($value*2==$max) {
+			$middle[] = $value;
+		} else {
+			$low[] = $value;
+		}
+	}
+	return array($surpass, $high, $middle, $low);
+}
+
+function match(array $a, $max){
+	$pack = array();
+	list($surpass, $high, $middle, $low) = half_group($a, $max);
+	if ($surpass) {
+		foreach ($surpass as $value) {
+			$pack[] = array($value);
+		}
+	}
+	// 处理high数组
+	if($high) {
+		foreach ($high as $value) {
+			$pack[] = get_company($value, $low, $max);
+		}
+	}
+	// 如果有中值，看个数
+	while ( !empty($middle) ){
+		$item = array_shift($middle);
+		if ($middle) {
+			$pack[] = array($item, array_shift($middle));
+		} else {
+			$pack[] = get_company($item, $low, $max);
+		}
+	}
+	// 低维数组如果还有值
+	if ($low) {
+		$pack[] = get_company(array_shift($low), $low, $max);
+	}
+	return $pack;
+}
+
+
+/**
+ * 从池子里找到不超过最大值的同伴
+ * 如果池子空了，直接返回最大值
+ */
+function get_company($big, array &$pool, $max){
+	$result = array($big);
+	if ( empty($pool) ) {
+		return $result;
+	}
+	foreach ($pool as $k=>$value) {
+		$sum = $big+$value;
+		// 如果和超过了最大值
+		if ($sum>$max){
+			continue;
+		}
+		$big = $sum;
+		// 放入袋子，并从池子里移出
+		unset($pool[$k]);
+		$result[] = $value;
+	}
+	return $result;
+}
+
+
+
